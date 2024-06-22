088e365c7aebe4a9fc1866af6e0c082ff97d624c
MinhKQ <minhkieu702@gmail.com>
2024-06-22 13:52:51 +0700

    fixbug

--- Word Finder/Assets/Scripts/ScriptableObjects/ObjectInGame/CategoryObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CategoryObject {
-    public string Category { get; set; }
-    public List<LevelObject> LevelOjects { get; set; } 
-}
